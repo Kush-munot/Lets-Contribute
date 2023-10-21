353fa7eb4c166306597be3bc3119279d8b56bd2a
Kush Munot <munot.kgm@gmail.com>
2023-10-21 20:02:54 +0530

    :zap:

--- Kush/main.cpp
+++ Kush/main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main()
 {
     cout << "Hello Kush here !!" << endl;
+    cout << "Hello Kush here !!" << endl;
+    cout << "Hello Kush here !!" << endl;
+    cout << "Hello Kush here !!" << endl;
+    cout << "Hello Kush here !!" << endl;
+    cout << "Hello Kush here !!" << endl;
     return 0;
 }
